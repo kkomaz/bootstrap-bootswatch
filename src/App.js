7e4a1aeb05bc601e85ecaad79436e94fbb1b0509
Alex Lee <alex@uniteus.com>
2020-07-06 11:57:13 -0400

    bootswatch add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootswatch/dist/minty/bootstrap.min.css'; // Added this :boom:
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <button className="btn btn-primary">
+          Primary
+        </button>
       </header>
     </div>
   );
